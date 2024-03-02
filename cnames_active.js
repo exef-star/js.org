103333c7ff53c42166ba8c1ccd64f05418e8e3d6
The hanta <150582140+exef-star@users.noreply.github.com>
2024-03-02 16:00:41 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -922,6 +922,7 @@ var cnames_active = {
   "dynamotion": "cname.vercel-dns.com", // noCF`
   "dyo": "dyo.github.io/dyo",
   "dyte": "dytejs.github.io",
+  "exef-star": "https://exef-star.github.io/hanta/",
   "e-query": "mjkhonline.github.io/e-query",
   "eagleeye": "webkrafters.github.io/eagleeye",
   "eahmed234": "eahmed234.github.io",
